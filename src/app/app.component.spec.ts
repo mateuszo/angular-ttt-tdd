cd04f361306cee22c4117be7f679c2710d62cb0d
Krzysztof Pawełczak <lbkrpaw87@gmail.com>
2021-07-01 14:49:57 +0200

    krzysiek

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,9 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { By } from '@angular/platform-browser';
-import { GameFieldComponent } from './components/game-field/game-field.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {By} from '@angular/platform-browser';
+import {GameFieldComponent} from './components/game-field/game-field.component';
+import {winningGameMoves} from './winning-conditions';
+import {allMoves} from './all-moves';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -19,6 +21,7 @@ describe('AppComponent', () => {
   describe('As player', () => {
     it(`I want to see 9 empty fields`, () => {
       const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
       const app = fixture.componentInstance;
       const fields = fixture.debugElement.queryAll(
         By.css('app-game-field button')
@@ -38,6 +41,7 @@ describe('AppComponent', () => {
 
     it('After click at "app-game-field" I want to see player "o" turn', () => {
       const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
       const turnField = fixture.debugElement.query(By.css('.app-game-turn'));
       const fields = fixture.debugElement.queryAll(
         By.css('app-game-field button')
@@ -49,6 +53,7 @@ describe('AppComponent', () => {
 
     it('After fulfilling win condition I want to see that the game is over', () => {
       const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
       const fields = fixture.debugElement.queryAll(
         By.css('app-game-field button')
       );
@@ -61,16 +66,57 @@ describe('AppComponent', () => {
       fields[2].triggerEventHandler('click', null);
       fixture.detectChanges();
 
+      const winningMoves = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+
       expect(
         turnField.nativeElement.classList.contains('game-ended')
       ).toBeTrue();
     });
+
     it('should check if there is no class `game-ended`', () => {
       const fixture = TestBed.createComponent(AppComponent);
       const turnField = fixture.debugElement.query(By.css('.app-game-turn'));
       expect(
         turnField.nativeElement.classList.contains('game-ended')
       ).toBeFalse();
+    });
+
+    it('Any winning condition should end the game', () => {
+
+      winningGameMoves.forEach((winningMoves: Array<number>) => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+
+        const fields = fixture.debugElement.queryAll(
+          By.css('app-game-field button')
+        );
+        const turnField = fixture.debugElement.query(By.css('.app-game-turn'));
+
+        const secondPlayerMoves = allMoves.filter((move: number) => !winningMoves.includes(move));
+
+        winningMoves.forEach((move: number, i: number) => {
+          fields[move].triggerEventHandler('click', null);
+          fields[secondPlayerMoves[i]].triggerEventHandler('click', null);
+          fixture.detectChanges();
+          if (i !== winningMoves.length - 1) {
+            expect(turnField.nativeElement.classList.contains('game-ended')).toBeFalse();
+          }
+        });
+
+        // fixture.detectChanges();
+
+        expect(turnField.nativeElement.classList.contains('game-ended')).toBeTrue();
+      });
+
     });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {winningGameMoves} from './winning-conditions';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,39 @@ export class AppComponent {
 
   title = 'ttt-tdd';
 
-  togglePlayer(): void {
+  fields = new Array(9);
+
+  moves = {
+    firstPlayer: [],
+    secondPlayer: []
+  };
+
+  togglePlayer(fieldIndex: number): void {
+    if (this.firstPlayer) {
+      this.moves.firstPlayer.push(fieldIndex);
+    } else {
+      this.moves.secondPlayer.push(fieldIndex);
+    }
     this.firstPlayer = !this.firstPlayer;
-    this.setGameEndClassList();
+    this.checkWiningConditions();
   }
 
   setGameEndClassList(): void {
     document.querySelector('.app-game-turn').classList.add('game-ended');
+  }
+
+  private checkWiningConditions(): void {
+    this.moves.firstPlayer.sort((a, b) => a - b);
+    this.moves.secondPlayer.sort((a, b) => a - b);
+
+    winningGameMoves.forEach((winningMoves: Array<number>) => {
+      if (JSON.stringify(winningMoves) === JSON.stringify(this.moves.firstPlayer)) {
+        this.setGameEndClassList();
+      }
+      if (JSON.stringify(winningMoves) === JSON.stringify(this.moves.secondPlayer)) {
+        this.setGameEndClassList();
+      }
+    });
+
   }
 }
